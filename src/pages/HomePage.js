c0cbca89eb279d03d276b238ffaa3ce8789f977a
Fitriawan Arya Nugraha <nugrahaaryaa76@gmail.com>
2024-01-29 20:08:51 +0700

    Ganti Nama

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -29,7 +29,7 @@ const HomePage = () => {
   return (
     <div ref={homeRef} className="container-home">
       <div id="home" className="HomeTitle">
-        <h2>Ciao!!, <br/>Your smile could literally <br/>melt my existance </h2>
+        <h2>Holaa!!, <br/>Your smile could literally <br/>melt my existance </h2>
         <p className="home-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
       </div>
     </div>
